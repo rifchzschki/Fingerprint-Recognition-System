94f480d722a41ad579ec961bedd0a4f66d8fc96c
TazakiN <tazkia.nizami@gmail.com>
2024-05-20 11:56:34 +0700

    feat: upload image

--- src/Tubes3/Views/MainWindow.axaml.cs
+++ src/Tubes3/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Tubes3.Views;
 
@@ -7,5 +10,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    [System.Obsolete]
+    private async void UploadButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            AllowMultiple = false,
+            Filters = new List<FileDialogFilter>
+                {
+                    new FileDialogFilter { Name = "Bitmap Images", Extensions = { "bmp" } }
+                }
+        };
+
+        var result = await openFileDialog.ShowAsync(this);
+        if (result != null && result.Length > 0)
+        {
+            var filePath = result[0];
+            if (File.Exists(filePath))
+            {
+                var bitmap = new Bitmap(filePath);
+                UploadedImage.Source = bitmap;
+            }
+        }
     }
 }
